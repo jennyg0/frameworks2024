bfa477403c5dda522c053800b77d499b86d663ec
jennyg0 <jen2348@hotmail.com>
2024-03-22 17:33:21 -0600

    update campaigns with project layout

--- src/app/campaigns/[projectId]/page.tsx
+++ src/app/campaigns/[projectId]/page.tsx
@@ -1,6 +1,6 @@
 import { getFrameMetadata } from "@coinbase/onchainkit/frame";
 import type { Metadata } from "next";
-import { NEXT_PUBLIC_URL } from "../config";
+import { NEXT_PUBLIC_URL } from "../../config";
 
 const frameMetadata = getFrameMetadata({
   buttons: [
