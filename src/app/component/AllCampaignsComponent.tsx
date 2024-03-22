1b9b3e8e1ca8a00e4b94264d8be7015d57d90e48
Xian Zheng <seeinplay@gmail.com>
2024-03-22 16:23:54 -0600

    initial campaign form and list

---
+++ src/app/component/AllCampaignsComponent.tsx
@@ -0,0 +1,7 @@
+export default function AllCampaignsComponent() {
+  return (
+    <div className='max-w-5xl mx-auto my-8 p-6 border rounded shadow-md'>
+      <h1 className='text-2xl font-bold text-center mb-6'>All Campaigns</h1>
+    </div>
+  );
+}
