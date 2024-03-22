3ae0d9693a8fec0e85e5558e5e30b142cce84dba
jennyg0 <jen2348@hotmail.com>
2024-03-22 16:45:11 -0600

    add example tx

---
+++ src/app/api/buttonclicker/index.ts
@@ -0,0 +1,46 @@
+import {
+  FrameRequest,
+  getFrameMessage,
+  getFrameHtmlResponse,
+} from "@coinbase/onchainkit/frame";
+import { NextRequest, NextResponse } from "next/server";
+import { encodeFunctionData, formatEther, parseEther, parseGwei } from "viem";
+import { baseSepolia } from "viem/chains";
+import type { FrameTransactionResponse } from "@coinbase/onchainkit/frame";
+import ClickTheButtonABI from "../../_contracts/ClickTheButtonAbi";
+import { BUY_MY_COFFEE_CONTRACT_ADDR } from "../../config";
+
+async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
+  const body: FrameRequest = await req.json();
+  const { isValid } = await getFrameMessage(body, {
+    neynarApiKey: "NEYNAR_ONCHAIN_KIT",
+  });
+
+  if (!isValid) {
+    return new NextResponse("Message not valid", { status: 500 });
+  }
+
+  // TODO
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = "force-dynamic";
+
+const data = encodeFunctionData({
+  abi: ClickTheButtonABI,
+  functionName: "clickTheButton",
+});
+
+const txData: FrameTransactionResponse = {
+  chainId: `eip155:${baseSepolia.id}`,
+  method: "eth_sendTransaction",
+  params: {
+    abi: [],
+    data,
+    to: BUY_MY_COFFEE_CONTRACT_ADDR,
+    value: parseGwei("10000").toString(), // 0.00001 ETH
+  },
+};
