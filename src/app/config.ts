3aea994d02a9f1a3472557d9e7db5832e0a364bd
jennyg0 <jen2348@hotmail.com>
2024-03-23 12:25:01 -0600

    update prod link

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : "https://prod-link.vercel.app";
+    : "https://frameworks2024.vercel.app";
 
 export const CROWDFUND_CONTRACT_ADDR =
   "0x392F6E583F0836Fd4ceC63D72eF4A24564810308";
