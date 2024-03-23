3fdb0331dbe2241e8309cb41ee9479c6ecad9e6a
Xian Zheng <seeinplay@gmail.com>
2024-03-23 16:10:38 -0600

    wagmi and viem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,21 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { PrivyProvider } from "@privy-io/react-auth";
 import { useRouter } from "next/navigation";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { base, baseSepolia } from "viem/chains";
+import { http, createConfig } from "wagmi";
+import { WagmiProvider } from "@privy-io/wagmi";
 
+const chains = [base, baseSepolia];
 const inter = Inter({ subsets: ["latin"] });
+const config = createConfig({
+  chains: [base, baseSepolia], // Pass your required chains as an array
+  transports: {
+    [base.id]: http(),
+    [baseSepolia.id]: http(),
+  },
+});
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -15,23 +28,24 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <PrivyProvider
-          appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
-          onSuccess={() => router.push("/campaigns")}
-          config={{
-            // Customize Privy's appearance in your app
-            appearance: {
-              theme: "dark",
-              accentColor: "#676FFF",
-              logo: "/next.svg",
-            },
-            embeddedWallets: {
-              createOnLogin: "users-without-wallets",
-            },
-          }}
-        >
-          {children}
-        </PrivyProvider>
+        <QueryClientProvider client={queryClient}>
+          <PrivyProvider
+            appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ""}
+            onSuccess={() => router.push("/campaigns")}
+            config={{
+              // Customize Privy's appearance in your app
+              appearance: {
+                theme: "dark",
+                accentColor: "#676FFF",
+              },
+              embeddedWallets: {
+                createOnLogin: "users-without-wallets",
+              },
+            }}
+          >
+            <WagmiProvider config={config}>{children}</WagmiProvider>
+          </PrivyProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
