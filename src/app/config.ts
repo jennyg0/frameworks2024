e6eaf2a0d8a15db8fbca4347d9ad62762a9ac910
jennyg0 <jen2348@hotmail.com>
2024-03-24 09:55:42 -0600

    update latest contract

--- src/app/config.ts
+++ src/app/config.ts
@@ -8,5 +8,3 @@ export const CROWDFUND_CONTRACT_ADDR =
   "0x08f27301b4446D4499C7Ec29875649b94C1595E6";
 
 export const BLOCKCHAIN_RPC_URL = "https://sepolia.base.org";
-
-// "https://rpc.notadegen.com/base/sepolia";
